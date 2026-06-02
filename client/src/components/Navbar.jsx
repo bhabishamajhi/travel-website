9130fc007534b0c1f561fbd29c5e20ea16e89491
bhabishamajhi <bhabishamajhi1@gmail.com>
2026-06-02 15:40:26 +0545

    update code

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         </ul>
 
         <div className="w-10 h-10 rounded-full border flex items-center justify-center">
-          <Link to="/login">
+          <Link to="/profile">
           👤
           </Link>
           
