aea328e02b4ec70b6f0d1532cbe158a57a4001ac
bhabishamajhi <bhabishamajhi1@gmail.com>
2026-06-08 20:12:32 +0545

    redeploy

--- client/src/pages/admin/Login.jsx
+++ client/src/pages/admin/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
 
   const navigate = useNavigate();
 
-  const API_URL = process.env.REACT_APP_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -81,7 +81,7 @@ const Login = () => {
             type="submit"
             disabled={loading}
             className="w-full bg-black text-white py-3 rounded-lg"
-          >
+          >         
             {loading ? "Logging in..." : "Login"}
           </button>
         </form>
